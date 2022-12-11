00f2af53d521c805a7f663c62ff2377a6d6143ce
sokelva <sokelva@mail.ru>
2022-12-11 17:32:16 +0300

    Модуль 5 Введение Задание 1.

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -10,6 +10,23 @@ namespace Module5
     {
         static void Main(string[] args)
         {
+            (string Name, string[] Dishes) User;
+
+
+            Console.WriteLine("Напишите свое имя: ");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо {0}: ", i+1);
+                User.Dishes[i] = Console.ReadLine();
+                
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
