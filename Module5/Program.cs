688ce807a071c1e4eba1d543fee636765641f60d
sokelva <sokelva@mail.ru>
2022-12-11 18:33:47 +0300

    Задание 5.1.5

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -24,9 +24,65 @@ namespace Module5
                 User.Dishes[i] = Console.ReadLine();
                 
             }
+            Console.WriteLine("Ваши любимые блюда: ");
+            foreach (var dish in User.Dishes)
+            {
+                Console.WriteLine(dish);
+            }
+            
+
+            string [] favcolors = new string [3];
+
+            for (int i = 0;i< favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            Console.WriteLine("Ваши любимые цвета: ");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
 
             Console.ReadKey();
 
         }
+
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+            
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+           return color; 
+        }
+
     }
 }
