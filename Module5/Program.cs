857a3ace12b848b51951c3a2bc6d7a30feb332ca
sokelva <sokelva@mail.ru>
2022-12-13 16:43:43 +0300

    Задание 5.3.11

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -10,17 +10,43 @@ namespace Module5
     {
         static void Main(string[] args)
         {
+            int data = 5;
             var arr = new int[] { 1, 2, 3 };
-            BigDataOperation(arr);
+            BigDataOperation(arr, data);
 
             Console.WriteLine(arr[0]);
+
+            Console.WriteLine("Введите свое имя: ");
+            //GetName();
             Console.ReadKey();
 
         }
 
-        static void BigDataOperation(in int[] arr)
+        static void BigDataOperation(in int[] arr,in int data) //in защищает данные от изменения/ используется лишь в случаях, когда изменять данные не нужно
         {
-            arr[0] = 4;
+            //data = 4;
+            arr[0] = data;
+        }
+
+        static void GetName(out string name, out string oldname)
+        {
+            oldname = "Евгения";
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+
+        }
+
+        static void GetAge(out string Name, out byte Age)
+        {
+            Name = "Елена";
+            Age = 42;
+        }
+
+        //num1, num2, num3, num4
+        static int SumNumbers(ref int num1, in int num2, out int num3, int num4)
+        {
+            num3 = (num1 + num2)* num4;
+            return num3;
         }
 
         //(string Name, string[] Dishes) User;
@@ -50,7 +76,7 @@ namespace Module5
         //    }
         //}
 
-       
+
 
     }
 }
