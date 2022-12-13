51a7f1a31dc179218e19f69f70b26a8e81844421
sokelva <sokelva@mail.ru>
2022-12-13 17:10:16 +0300

    Области видимости

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -76,6 +76,28 @@ namespace Module5
         //    }
         //}
 
+        static void Main2(string[] args)
+        {
+            var num = 1;
+
+            Matreshka(num);
+
+            Console.ReadKey();
+
+        }
+
+        static int Matreshka(int num)
+        {
+            num++;
+
+            return Sum(num, 3);
+        }
+
+        static int Sum(int num, int num2)
+        {
+            return num + num2;
+        }
+
 
 
     }
