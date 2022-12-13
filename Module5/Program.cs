28f4904e9c0611dc80d13ed1596730cf3e01d38e
sokelva <sokelva@mail.ru>
2022-12-13 16:10:16 +0300

    Задание 5.3.6

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -10,193 +10,47 @@ namespace Module5
     {
         static void Main(string[] args)
         {
-            (string Name, string[] Dishes) User;
+            var arr = new int[] { 1, 2, 3 };
+            BigDataOperation(arr);
 
-            //int [] array = new int[3];
-            //int[] array = GetArrayFromConsole();
-            var array = GetArrayFromConsole(10);
-            Console.ReadKey();
-
-            GetArrayFromConsole();
-
-            #region Tuple
-            var (name, age) = ("Елена", 42);
-            Console.WriteLine("Введите имя: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Введите возраст: ");
-            age = int.Parse(Console.ReadLine());
-            #endregion
-
-
-            Console.WriteLine("Напишите свое имя: ");
-            User.Name = Console.ReadLine();
-
-            User.Dishes = new string[5];
-
-            for (int i = 0; i < User.Dishes.Length; i++)
-            {
-                Console.WriteLine("Введите любимое блюдо {0}: ", i + 1);
-                User.Dishes[i] = Console.ReadLine();
-
-            }
-            Console.WriteLine("Ваши любимые блюда: ");
-            foreach (var dish in User.Dishes)
-            {
-                Console.WriteLine(dish);
-            }
-
-
-            string[] favcolors = new string[3];
-
-            for (int i = 0; i < favcolors.Length; i++)
-            {
-                favcolors[i] = ShowColor(name, age);
-            }
-
-            Console.WriteLine("Ваши любимые цвета: ");
-            foreach (var color in favcolors)
-            {
-                Console.WriteLine(color);
-            }
-
-
-            //int [] array = new int[3];
-            //int[] array = GetArrayFromConsole();
-            //var array = GetArrayFromConsole(3);
+            Console.WriteLine(arr[0]);
             Console.ReadKey();
 
         }
 
-        static string ShowColor(string username, int userage)//, string [] favcolors
+        static void BigDataOperation(in int[] arr)
         {
-            Console.WriteLine("{0} {1} лет,\nнапишите свой любимый цвет на английском с маленькой буквы", username, userage);
-            var color = Console.ReadLine();
-
-            switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
-            }
-            return color;
+            arr[0] = 4;
         }
 
-        //static void ShowColors(string[] favcolors)
+        //(string Name, string[] Dishes) User;
+
+        //Console.WriteLine("Напишите свое имя: ");
+        //User.Name = Console.ReadLine();
+
+        //User.Dishes = new string[5];
+
+        //for (int i = 0; i < User.Dishes.Length; i++)
         //{
-        //    Console.WriteLine("Ваши любимые цвета:");
-        //    foreach (var color in favcolors)
+        //    Console.WriteLine("Введите любимое блюдо {0}: ", i + 1);
+        //    User.Dishes[i] = Console.ReadLine();
+
+        //}
+
+        //GetDishes(User.Dishes, ref User.Name);
+
+        //}
+
+        //static void GetDishes(string [] dishes, ref string name)
+        //{
+        //    Console.WriteLine("Ваши любимые блюда: ");
+        //    foreach (var dish in dishes)
         //    {
-        //        Console.WriteLine(color);
+        //        Console.WriteLine(dish);
         //    }
         //}
 
-        //метод возвращает массив
-
-        static int[] GetArrayFromConsole(int num = 3)
-        {
-            var result = new int[num];
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
-
-            //int[] sortResult = SortArray(result);
-
-            //foreach (var item in sortResult)
-            //{
-            //    Console.WriteLine("Элементы массива: {0}", item);
-            //}
-            result = ShowArray(result, true);
-            Console.ReadKey();
-
-            return result;
-        }
-
-        static int[] ShowArray(int[] array, bool isSort = false)
-        {
-            var temp = array;
-            if (isSort)
-            {
-                temp = SortArray(array);
-            }
-
-            foreach(var item in temp)
-            {
-                Console.WriteLine("Элементы массива: {0}", item);
-            }
-            Console.ReadKey();
-            return temp;
-        }
-
-        static int[] SortArray(int[] result)
-        {
-            Array.Sort(result);
-            return result;
-        }
-
-        //Массив параметров
-
-        //static void ShowColors(name, favcolors)
-        //{
-        //    Console.WriteLine("Ваши любимые цвета:");
-        //    foreach (var color in favcolors)
-        //    {
-        //        Console.WriteLine(color);
-        //    }
-        //}
-
-
-        static void SortComplexArray(int[,] arr)
-        {
-            int temp;
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
-            {
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
-                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
-                        if (arr[i, j] > arr[i, k])
-                        {
-                            temp = arr[i, k];
-                            arr[i, k] = arr[i, j];
-                            arr[i, j] = temp;
-                        }
-            }
-        }
-
-        static void Sort(string[] args)
-        {
-
-            int[,] arr = {
-                    { - 5,6,9,1,2,-3},
-                    { - 8,8,1,1,2,-3}
-                  };
-
-            SortComplexArray(arr);
-
-        }
+       
 
     }
 }
