cfd478e43a4b3a222867e7bb5e09c2970eae889b
sokelva <sokelva@mail.ru>
2022-12-13 18:00:38 +0300

    Задание 5.5.8

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -4,6 +4,8 @@ class MainClass
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(PowerUp(2,6));
+
         Console.WriteLine("Напишите что-то");
         var str = Console.ReadLine();
 
@@ -12,6 +14,7 @@ class MainClass
 
         Echo(str, deep);
 
+        Console.WriteLine(Factorial(20));
         Console.ReadKey();
     }
 
@@ -22,11 +25,43 @@ class MainClass
         {
             modif = modif.Remove(0, 2).Trim();
         }
-        Console.WriteLine(modif);
+        Console.BackgroundColor = (ConsoleColor)deep;
+        Console.WriteLine("..." + modif);
 
         if (deep > 1)
         {
             Echo(modif, deep - 1);
+        }
+    }
+
+    static decimal Factorial(decimal x)
+    {
+        if (x == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+    }
+
+    private static int PowerUp(int N, byte pow)
+    {
+        if (pow == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
         }
     }
 }
